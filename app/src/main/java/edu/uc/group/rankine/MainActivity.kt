c1c31dc48c4c9570bda572bfec59e0863fdb5839
Eric Davin <davinec@mail.uc.edu>
2021-01-31 13:43:51 -0500

    blank project

---
+++ app/src/main/java/edu/uc/group/rankine/MainActivity.kt
@@ -0,0 +1,18 @@
+package edu.uc.group.rankine
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import edu.uc.group.rankine.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/edu/uc/group/rankine/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package edu.uc.group.rankine.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
