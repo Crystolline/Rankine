e867f7fc6630c01f94e376a4131f5a94befaa2f9
Eric Davin <davinec@mail.uc.edu>
2021-02-11 20:23:16 -0500

    added ui

--- app/src/main/java/edu/uc/group/rankine/MainActivity.kt
+++ app/src/main/java/edu/uc/group/rankine/MainActivity.kt
@@ -2,7 +2,7 @@ package edu.uc.group.rankine
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import edu.uc.group.rankine.ui.main.MainFragment
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,7 +11,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.main_activity)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance())
                     .commitNow()
         }
     }
