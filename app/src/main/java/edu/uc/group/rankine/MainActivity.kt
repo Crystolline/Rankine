ec1df75200805b1f84f4c8931652d339a36017a2
Eric Davin <davinec@mail.uc.edu>
2021-02-15 17:22:43 -0500

    added toolbar

--- app/src/main/java/edu/uc/group/rankine/MainActivity.kt
+++ app/src/main/java/edu/uc/group/rankine/MainActivity.kt
@@ -2,16 +2,22 @@ package edu.uc.group.rankine
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import androidx.appcompat.widget.Toolbar
+import edu.uc.group.rankine.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
                     .commitNow()
         }
+
+
     }
 }
