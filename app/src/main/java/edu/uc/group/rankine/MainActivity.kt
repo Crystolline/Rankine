67db7a88388c762a5d5052544c8db4ca48a5b50c
Eric Davin <davinec@mail.uc.edu>
2021-02-15 23:11:01 -0500

    query text?

--- app/src/main/java/edu/uc/group/rankine/MainActivity.kt
+++ app/src/main/java/edu/uc/group/rankine/MainActivity.kt
@@ -8,6 +8,7 @@ import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import edu.uc.group.rankine.ui.main.MainFragment
+import androidx.appcompat.widget.SearchView
 
 class MainActivity : AppCompatActivity() {
 
@@ -27,10 +28,26 @@ class MainActivity : AppCompatActivity() {
         menuInflater.inflate(R.menu.toolbar_menu, menu)
         val manager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
         val searchItem = menu?.findItem(R.id.search_icon)
-        val searchView = searchItem?.actionView as androidx.appcompat.widget.SearchView
+        val searchView = searchItem?.actionView as SearchView
+
+        searchView.setSearchableInfo(manager.getSearchableInfo(componentName))
+
+        searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                searchView.clearFocus()
+                searchView.setQuery(query , false)
+                searchItem.collapseActionView()
+                Toast.makeText(this@MainActivity, "looking for $query", Toast.LENGTH_LONG).show()
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
+            }
 
 
-        return super.onCreateOptionsMenu(menu)
+        })
+        return true
     }
 
 
