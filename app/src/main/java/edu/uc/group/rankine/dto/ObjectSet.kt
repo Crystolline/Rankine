05a9692e0f344feee636e17ab4279c5485134785
crystolline <colinsmcqueen@gmail.com>
2021-03-01 10:11:38 -0500

    Document DTOs

--- app/src/main/java/edu/uc/group/rankine/dto/ObjectSet.kt
+++ app/src/main/java/edu/uc/group/rankine/dto/ObjectSet.kt
@@ -1,22 +1,31 @@
 package edu.uc.group.rankine.dto
 
+/**
+ * An unranked set of <elements> alongside a list of <fields> to define attributes for individual elements
+ */
 data class ObjectSet(var elements: ArrayList<ElementObject> = ArrayList(), var fields: ArrayList<FieldObject> = ArrayList())
 {
 
+    /**
+     * Add element <e> to the ObjectSet's element list
+     */
     fun addElement(e: ElementObject)
     {
         elements.add(e)
 
     }
 
-
+    /**
+     * Add field <f> to the ObjectSet's fields list
+     */
     fun addField(f: FieldObject)
     {
         fields.add(f)
     }
 
-
-
+    /**
+     * Modify an element <inputObject> in the ObjectSet with the name and attributes of element <newObject>
+     */
     fun modifyElements(inputObject: ElementObject, newObject : ElementObject)
     {
         //Eventually should bring up a list of each field
@@ -30,6 +39,9 @@ data class ObjectSet(var elements: ArrayList<ElementObject> = ArrayList(), var f
 
     }
 
+    /**
+     * Modify a field <inputObject> in the ObjectSet with the name and datatype of field <newObject>
+     */
     fun modifyFields(inputObject: FieldObject, newObject : FieldObject)
     {
         //Eventually should bring up a list of each field
